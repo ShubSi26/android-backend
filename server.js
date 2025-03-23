10caa67656ddc7cb8d7ad5a5ec25c4deef9a30d2
shubhamgit <shubhamsi160@gmail.com>
2025-03-23 17:34:34 +0530

    /ping route

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const app = express();
 app.use(express.json());
 
 app.use('/api', router);
+app.use('/ping', (req, res) => {
+    res.status(200).json({ message: 'pong' });
+});
 
-app.listen(3000, () => {
+app.listen(80, () => {
     console.log('Server is running on port 3000');
 });
 
